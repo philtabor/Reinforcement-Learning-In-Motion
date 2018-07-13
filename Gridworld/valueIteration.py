2af4062d78ecb9dcbf0f8fb1a2999664aa13a289
MachineLearningLab-AI <ptabor@gmail.com>
2018-07-12 21:49:33 -0700

    changed variables for clarity

--- Gridworld/valueIteration.py
+++ Gridworld/valueIteration.py
@@ -21,16 +21,16 @@ def iterateValues(grid, V, policy, GAMMA, THETA):
             converged = True if DELTA < THETA else False
 
     for state in grid.stateSpace:
-        actionValues = []
+        newValues = []
         actions = []
         for action in grid.actionSpace:
             for key in grid.p:
                 (newState, reward, oldState, act) = key
                 if state == oldState and action == act:
-                    actionValues.append(grid.p[key]*(reward+GAMMA*V[newState]))
+                    newValues.append(grid.p[key]*(reward+GAMMA*V[newState]))
             actions.append(action)
-        actionValues = np.array(actionValues)
-        bestActionIDX = np.where(actionValues == actionValues.max())[0]        
+        newValues = np.array(newValues)
+        bestActionIDX = np.where(newValues == newValues.max())[0]        
         #bestActions = actions[np.random.choice(bestActionIDX)]
         bestActions = actions[bestActionIDX[0]]
         policy[state] = bestActions
--- Gridworld/valueIterationWithMoves.py
+++ Gridworld/valueIterationWithMoves.py
@@ -21,16 +21,16 @@ def iterateValues(grid, V, policy, GAMMA, THETA):
             converged = True if DELTA < THETA else False
 
     for state in grid.stateSpace:
-        actionValues = []
+        newValues = []
         actions = []
         for action in grid.actionSpace:
             grid.setState(state)
             newState, reward, _, _ = grid.step(action)
             key = (newState, reward, state, action)
-            actionValues.append(grid.p[key]*(reward+GAMMA*V[newState]))
+            newValues.append(grid.p[key]*(reward+GAMMA*V[newState]))
             actions.append(action)
-        actionValues = np.array(actionValues)
-        bestActionIDX = np.where(actionValues == actionValues.max())[0]
+        newValues = np.array(newValues)
+        bestActionIDX = np.where(newValues == newValues.max())[0]
         #bestActions = actions[np.random.choice(bestActionIDX)]
         bestActions = actions[bestActionIDX[0]]
         policy[state] = bestActions
