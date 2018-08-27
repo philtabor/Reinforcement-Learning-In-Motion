c783dd74c618fa9da8e0e1d47698a4cf8b13dce1
MachineLearningLab-AI <ptabor@gmail.com>
2018-08-26 20:54:24 -0700

    removed plot

--- Unit-7-The-Cartpole/tdZero.py
+++ Unit-7-The-Cartpole/tdZero.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import gym
 
 def SimplePolicy(state):
@@ -18,8 +17,7 @@ if __name__ == '__main__':
     for state in range(len(states)+1):
         V[state] = 0
 
-    totalRewards = []
-    for i in range(1000):
+        for i in range(1000):
         # cart x position, cart velocity, pole theta, pole velocity
         observation = env.reset()
         done = False
@@ -27,15 +25,10 @@ if __name__ == '__main__':
         while not done:      
             s = int(np.digitize(observation[2], states))
             a = SimplePolicy(s)            
-            observation_, reward, done, info = env.step(a)
-            epRewards += reward
+            observation_, reward, done, info = env.step(a)            
             s_ = int(np.digitize(observation_[2], states))            
             V[s] = V[s] + ALPHA*(reward + GAMMA*V[s_] - V[s])
             observation = observation_
-        totalRewards.append(epRewards)
     
     for state in V:
         print(state, '%.3f' % V[state])
-    
-    plt.plot(totalRewards)
-    plt.show()
